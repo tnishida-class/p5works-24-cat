49eda57c02a4b459294ddd59b190cd0037d1dd7a
24-cat <72426620+24-cat@users.noreply.github.com>
2020-10-13 12:13:34 +0900

    section1-1 :re
    send you again

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,11 +5,12 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+
+  fill(278, 138, 122);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
-  fill(255);
+  fill(123, 242, 98);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
