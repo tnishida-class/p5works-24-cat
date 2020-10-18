489ab55e6f4284ddb356bba34acf203615ea5b9f
24-cat <72426620+24-cat@users.noreply.github.com>
2020-10-18 21:51:17 +0900

    Update practice2-2.js

--- practice/practice2-2.js
+++ practice/practice2-2.js
@@ -3,12 +3,12 @@ function setup() {
   background(196);
   fill(0);
 
-  for(let i = 0; i < 9; i++){
+  for(let i = 0; i < 9; i++) {
     line(i, 0 , i, 100);
-    if(i < 4){
+    if(i < 4) {
       strokeWeight(2);
     }
-    else(i > 4){
+    else(i > 4) {
       storkeWeight(1);
     }
   }
