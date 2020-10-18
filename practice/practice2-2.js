9159b57c3f28ea5b78864eefd892dae86c599f6f
24-cat <72426620+24-cat@users.noreply.github.com>
2020-10-18 16:51:03 +0900

    practice2-2

--- practice/practice2-2.js
+++ practice/practice2-2.js
@@ -1,5 +1,12 @@
 function setup(){
   createCanvas(100,100);
-  background(196);
-  line(10, 0, 10, 100)
+  background(0);
+  let x = 10;
+  while(x <= 90){
+    line(x, 0, x, 100);
+    x = x + 10;
+  // }
+  // for(x = 10, x <= 90, x += 10){
+    // line(x, 0, x, 100)
+  }
 }
