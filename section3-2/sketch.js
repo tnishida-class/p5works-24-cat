d4c7e92a765ee1b821ae66703632c0a6fd92ec16
24-cat <72426620+24-cat@users.noreply.github.com>
2020-10-27 22:54:17 +0900

    Update sketch.js

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -6,7 +6,7 @@ function setup(){
   fill(0);
   crossmark(10, 10, 90, 90);
   ngmark(150, 50, 80);
-  star(250, 50, 40);
+  regularPolygon(250, 50, 40, 40);
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -24,7 +24,7 @@ function ngmark(cx, cy, r){
   pop();
 }
 
-function star(cx, cy, r){
+function regularPolygon(n, cx, cy, r){
   beginShape();
   for(var i = 0; i < 5; i++){
     let theta = TWO_PI * i * 2 / 5 - HALF_PI;
