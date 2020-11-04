0aebbc59c9079eb55cff590322464da9a2c597b4
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-04 16:19:22 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,12 +4,14 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
+  noStroke();
   let c = color(0, 0, 255)
   let d = color(0, 255, 0)
-  balloon("I love keyakizaka46", c, d, 80, 160, 240, 80);
+  let e = color(255, 0, 0)
+  balloon("I love keyakizaka46", c, d, e, 80, 160, 240, 80, 20);
 }
 
-function balloon(t, c0, c1, x, y, w0, h0){
+function balloon(t, c0, c1, c2, x, y, w0, h0, l0){
   // let w = textWidth(t);
   // let h = textAscent() + textDescent();
   // let p = 2;
@@ -18,4 +20,6 @@ function balloon(t, c0, c1, x, y, w0, h0){
   rect()
   fill(c1);
   text(t, x + w0 / 4, y + h0 / 2); // これyは下だな　
+  fill(c2);
+  triangle(x + w0, y + h0, x + w0, y + h0 + l0, x + w0 - l0, y + h0)
 }
