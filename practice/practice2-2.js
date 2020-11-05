060d79a2c78b23a5df324e9b07db3c9485debc66
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-05 17:34:46 +0900

    Update practice2-2.js

--- practice/practice2-2.js
+++ practice/practice2-2.js
@@ -1,49 +1,14 @@
-// アメリカの国旗途中
 function setup(){
-  createCanvas(270, 180);
-  fill(0);
-  let h = height / 13;
-  for(let i = 0; i < 13; i++){
-    if(i % 2 == 0){
-      fill(255, 0, 0)
-      rect(0, i * h, width, h);
-    }
-    else{
-      fill(255);
-      rect(0, i * h, width, h);
-    }
-  }
-  noStroke();
-  fill(0, 0, 255)
-  rect(0, 0, width * 3 / 7, 7 * h);
-  fill(255);
-  for(let j = 0; j < 6; j++){
-    for(let k = 0; k < 5; k++){
-      let w = width / 14;
-      let hh = height / 8.5;
-      let x = j * w + w * 0.5;
-      let y = k * hh + hh * 0.3;
-      star(x, y, 5);
-    }
-  }
-  for(let j = 0; j < 5; j++){
-    for(let k = 0; k < 4; k++){
-      let w = width / 14;
-      let hh = height / 8.5;
-      let x = (j + 0.5) * w + w * 0.5;
-      let y = (k + 0.5) * hh + hh * 0.3;
-      star(x, y, 5);
-    }
-  }
+  createCanvas(400, 400);
+  kobeCity(50, 50, 40);
 }
-
-function star(cx, cy, r){
-  beginShape();
-  for(i = 0; i < 5; i++){
-      const theta = TWO_PI * i * 2/5 - HALF_PI;
-      const p = cx + cos(theta) * r;
-      const q = cy + sin(theta) * r;
-      vertex(p, q);
-  }
-  endShape();
+function kobeCity(x, y, wh){ // whはw と hの意味
+  push();
+  noFill();
+  strokeWeight(wh * 0.5);
+  ellipseMode(RADIUS);
+  strokeCap(SQUARE);
+  arc(x, y, wh, wh, PI + PI / 4, PI / 4 + TWO_PI);
+  arc(x + wh * 1.25, y, wh, wh, PI * 3 / 4, PI * 3 / 4 + PI);
+  pop();
 }
