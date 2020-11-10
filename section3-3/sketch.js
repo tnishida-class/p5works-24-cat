d9a3f30b05b1ee74799cb4c124dbfb751fc903bd
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-10 12:20:46 +0900

    Update sketch.js

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -17,14 +17,15 @@ function calendar(y, m){
   let dow = dayOfWeek(y, m, 1);
   for(let d = 1; d <= daysInMonth(y, m); d++){
     // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+    
   }
 }
 
 function isLeapYear(y){
   return (y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0);
-}
+} // うるう年かどうか
 
-function daysInYear(y){
+function daysInYear(y){ // 1年当たりの日数
   // BLANK[1]
   if(isLeapYear(y)){
     return 366;
@@ -34,7 +35,7 @@ function daysInYear(y){
   }
 }
 
-function daysInMonth(y, m){
+function daysInMonth(y, m){ // ひと月当たりの日数
   if(m == 2){
     return isLeapYear(y) ? 29 : 28;
   }
@@ -46,7 +47,7 @@ function daysInMonth(y, m){
   }
 }
 
-function dayOfYear(y, m, d){
+function dayOfYear(y, m, d){ // ある年における日にち
   let count = 0;
   for(let i = 1; i < m; i++){
     count += daysInMonth(y, i);
@@ -54,11 +55,19 @@ function dayOfYear(y, m, d){
   return count + d;
 }
 
-function dayOfWeek(y, m, d){
+function dayOfWeek(y, m, d){ // ある週における日にち
   // BLANK[2]
+  const ys = 1970 // s : year standard 1970.01.01(Thu.)
+  let count = 0;
+  for(i = 1970; i < y; i++){
+    count + daysInYear(i);
+  }
+  let ds = count + dayOfYear(y, m, d); // ds = day sum
+    return ds % 7;
+  }
 }
 
 function dayOfWeekAsString(dow){
-  const a = ["日", "月", "火", "水", "木", "金", "土", "日"];
+  const a = ["木", "金", "土", "日", "月", "火", "水", "木"];
   return a[dow];
 }
