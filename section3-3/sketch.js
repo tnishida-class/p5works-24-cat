1b5abff29c7843304f52ebce271d560ea2d3c78e
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-10 13:07:47 +0900

    Update sketch.js

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,6 +1,6 @@
 // テキスト「関数を作る(2) 値を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(500, 500);
   calendar(2019, 10);
 
   for(let i = 2000; i <= 2100; i++){
@@ -14,10 +14,36 @@ function setup(){
 }
 
 function calendar(y, m){
+  text("y年 m月", width / 3, 25); // yとmを引数で表示するにはどうすれば？
   let dow = dayOfWeek(y, m, 1);
+  for(let i = 0; i < 35; i++){
+    if(i % 7 == 0){
+      text("日", width / 35 * i, 50);
+    }
+    else if(i % 7 == 1){
+      text("月", width / 35 * i, 50);
+    }
+    else if(i % 7 == 2){
+      text("火", width / 35 * i, 50);
+    }
+    else if(i % 7 == 3){
+      text("水", width / 35 * i, 50);
+    }
+    else if(i % 7 == 4){
+      text("木", width / 35 * i, 50);
+    }
+    else if(i % 7 == 5){
+      text("金", width / 35 * i, 50);
+    }
+    else if(i % 7 == 6){
+      text("土", width / 35 * i, 50);
+    }
+  }
   for(let d = 1; d <= daysInMonth(y, m); d++){
     // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
-    
+    if(dow == 0){
+      text("d", width / 14 * d, height / 2);
+    }
   }
 }
 
@@ -63,11 +89,10 @@ function dayOfWeek(y, m, d){ // ある週における日にち
     count + daysInYear(i);
   }
   let ds = count + dayOfYear(y, m, d); // ds = day sum
-    return ds % 7;
+    return ds % 7 + 3;
   }
-}
 
 function dayOfWeekAsString(dow){
-  const a = ["木", "金", "土", "日", "月", "火", "水", "木"];
+  const a = ["日", "月", "火", "水", "木", "金", "土", "日"];
   return a[dow];
 }
