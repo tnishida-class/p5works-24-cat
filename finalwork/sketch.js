1c3d33e92091a3f6759704fd0e50bc2b0e4178a0
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-18 17:17:16 +0900

    update

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -3,6 +3,10 @@ let img1, img2, img3, img4, img5;
 function preload(){
   img1 = loadImage('Eye.jpg');
   img2 = loadImage('Ear.jpg');
+  img3 = loadImage('Nose.jpg');
+  img4 = loadImage('Sun.png');
+  img5 = loadImage('Speaker.png');
+  img6 = loadImage('Food.png');
 }
 
 function setup(){
@@ -11,8 +15,12 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  image(img1, width / 5 * 2, 0);
-  image(img2, width / 5 * 2, height / 3);
+  image(img1, width / 5 * 2, 0, width / 10, height / 5);
+  image(img2, width / 5 * 2, height / 5 * 2, width / 10, height / 5);
+  image(img3, width / 5 * 2, height / 5 * 4, width / 10, height / 5);
+  image(img4, 0, 0, width / 10, height / 5);
+  image(img5, 0, height / 5 * 2, width / 10, height / 5);
+  image(img6, 0, height / 5 * 4, width / 10, height / 5);
 }
 
 function windowResized(){
