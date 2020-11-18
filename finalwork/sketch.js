7e39b95da400875076769472e160e45a92309f85
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-18 16:52:57 +0900

    update

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,18 +1,24 @@
 // 最終課題を制作しよう
 let img1, img2, img3, img4, img5;
 function preload(){
-  img1 = loadImage('cat.jpg');
+  img1 = loadImage('Eye.jpg');
+  img2 = loadImage('Ear.jpg');
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  image(img1, width / 2, height / 2);
 }
 
 function draw(){
   background(160, 192, 255);
+  image(img1, width / 5 * 2, 0);
+  image(img2, width / 5 * 2, height / 3);
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function arrow(){
+
 }
--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -5,21 +5,24 @@ let cycle;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 200;
 }
 
 function draw(){
   background(200, 200, 0);
-  count = (count + 1) % cycle;
+  count = (count + 2) % cycle;
   // BLANK[1]
   let size;
-  // if(count < cycle / 2){
-  //   size = count;
-  // }
-  // else{
-  //   size = cycle - count;
-  // }
-  size = constrain(count, 50, 70);
-  fill(255, 0, 0)
-  ellipse(width / 2, height / 2, size);
+  if(count < cycle / 2){
+    size = count;
+  }
+  else{
+    size = cycle - count;
+  }
+  // size = constrain(count, 50, 70);
+  fill(255, 0, 0);
+  arc(width / 2, height / 2, size, size, PI + HALF_PI + PI / 8, TWO_PI + HALF_PI + PI / 8);
+  fill(0, 0, 255);
+  arc(width / 2, height / 2, size, size, HALF_PI + PI / 8, PI + HALF_PI + PI / 8);
 }
+// 心臓っぽいものが分からなかったので2つ用意しておりました。出していなかった方を表示し直しました。
