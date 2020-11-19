055fe0b1068f7dd4d914783eda7491d153aaea4d
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-19 12:28:21 +0900

    update

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,9 +1,9 @@
 // 最終課題を制作しよう
 let img1, img2, img3, img4, img5;
 function preload(){
-  img1 = loadImage('Eye.jpg');
-  img2 = loadImage('Ear.jpg');
-  img3 = loadImage('Nose.jpg');
+  img1 = loadImage('Eye.png');
+  img2 = loadImage('Ear.png');
+  img3 = loadImage('Nose.png');
   img4 = loadImage('Sun.png');
   img5 = loadImage('Speaker.png');
   img6 = loadImage('Food.png');
@@ -16,19 +16,121 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  image(img1, width / 4 * 1, 0, width / 10, height / 5);
-  image(img2, width / 4 * 1, height / 5 * 2, width / 10, height / 5);
-  image(img3, width / 4 * 1, height / 5 * 4, width / 10, height / 5);
-  image(img4, 0, 0, width / 10, height / 5);
-  image(img5, 0, height / 5 * 2, width / 10, height / 5);
-  image(img6, 0, height / 5 * 4, width / 10, height / 5);
-  image(img7, width / 3 * 2, height / 8 * 1, width / 3, height / 4 * 3);
+  push();
+  stroke(0);
+  text("感覚が生じる仕組み　簡便図", width / 3 * 2, height / 8 * 1);
+  text("A", 0, height / 40);
+  text("B", 0, height / 5 * 2 + height / 40);
+  text("C", 0, height / 5 * 4 + height / 40);
+  pop();
+  image(img1, width / 4 * 1, 0, width / 10, height / 5); // eye
+  image(img2, width / 4 * 1, height / 5 * 2, width / 10, height / 5); // ear
+  image(img3, width / 4 * 1, height / 5 * 4, width / 10, height / 5); // nose
+  image(img4, 0, 0, width / 10, height / 5); // sun
+  image(img5, 0, height / 5 * 2, width / 10, height / 5); // speaker
+  image(img6, 0, height / 5 * 4, width / 10, height / 5); // food
+  image(img7, width / 3 * 2, height / 8 * 1, width / 3, height / 4 * 3); // brain
+  line(width / 20 * 7, height / 10, width / 100 * 95, height / 2); // eye
+  line(width / 20 * 7, height / 2, width / 100 * 90, height / 3); // ear
+  line(width / 20 * 7, height / 10 * 9, width / 100 * 70, height / 2); // nose
+  KeyPressed();
+  noFill();
+  ellipse(width / 20 * 13, height / 10 * 3, height / 10); // eye
+  ellipse(width / 8 * 5, height / 12 * 5, height / 10); // ear
+  ellipse(width / 40 * 21, height / 10 * 7, height / 10); // nose
+  let x = mouseX; // 長くなるので変数宣言し代入
+  let y = mouseY;
+  let w = width;
+  let h = height;
+  if(x > w / 20 * 13 - h / 20 && x < w / 20 * 13 + h / 20 && y > h / 10 * 3 - h / 20 && y < h / 10 * 3 + h / 20){
+    push();
+    fill(255, 255, 0);
+    ellipse(width / 20 * 13, height / 10 * 3, height / 10); // eye
+    text("視覚in後頭葉", width / 100 * 90, height / 2);
+    pop();
+    push();
+    stroke(0);
+    text("特殊神経エネルギー説に基づくと", width / 3 * 2, height / 8 * 2);
+    pop();
+  }
+  if(x > w / 8 * 5 - h / 20 && x < w / 8 * 5 + h / 20 && y > h / 12 * 5 - h / 20 && y < h / 12 * 5 + h / 20){
+    push();
+    fill(0, 0, 255);
+    ellipse(width / 8 * 5, height / 12 * 5, height / 10); // ear
+    text("聴覚in側頭葉", width / 100 * 85, height / 3);
+    pop();
+    push();
+    stroke(0);
+    text("特殊神経エネルギー説に基づくと", width / 3 * 2, height / 8 * 2);
+    pop();
+  }
+  if(x > w / 40 * 21 - h / 20 && x < w / 40 * 21 + h / 20 && y > h / 10 * 7 - h / 20 && y < h / 10 * 7 + h / 20){
+    push();
+    fill(150, 0, 255);
+    ellipse(width / 40 * 21, height / 10 * 7, height / 10); // nose
+    text("嗅覚in大脳？？？", width / 100 * 65, height / 2);
+    pop();
+    push();
+    stroke(0);
+    text("特殊神経エネルギー説に基づくと", width / 3 * 2, height / 8 * 2);
+    pop();
+  }
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function arrow(){
+function arrow(x, y){ // ? / 20 ずつふやしていけばよさそう
+  beginShape();
+  push();
+  noStroke();
+  vertex(x, y);
+  vertex(x + width / 20, y);
+  vertex(x + width / 20, y + height / 40);
+  vertex(x + width / 40 * 3, y - height / 40); // 矢印の先端
+  vertex(x + width / 20, y - height / 40 * 3);
+  vertex(x + width / 20, y - height / 20);
+  vertex(x, y - height / 20);
+  vertex(x, y);
+  endShape();
+  pop();
+}
 
+function KeyPressed(){ // 反応しない，drawと変数を対応させないとダメなのかな
+  // KeyPressed();をdrawに置いたらできた。
+  if(key == "a"){
+    // 四角Aにもなんか処理したい
+    push();
+    fill(255, 255, 0);
+    arrow(width / 20 * 3, height / 10); //
+    ellipse(width / 20 * 13, height / 10 * 3, height / 10); // eye
+    text("視覚in後頭葉", width / 100 * 90, height / 2);
+    strokeWeight(10);
+    stroke(0)
+    text("A", 0, height / 40);
+    pop();
+  }
+  else if(key == "b"){
+    push();
+    fill(0, 100, 255);
+    arrow(width / 20 * 3, height / 2);
+    ellipse(width / 8 * 5, height / 12 * 5, height / 10); // ear
+    text("聴覚in側頭葉", width / 100 * 85, height / 3);
+    strokeWeight(10);
+    stroke(0)
+    text("B", 0, height / 5 * 2 + height / 40);
+    pop();
+  }
+  else if(key == "c"){
+    push();
+    fill(150, 0, 255);
+    arrow(width / 20 * 3, height / 10 * 9);
+    ellipse(width / 40 * 21, height / 10 * 7, height / 10); // nose
+    text("嗅覚in大脳？？？", width / 100 * 65, height / 2);
+    strokeWeight(10);
+    stroke(0)
+    text("C", 0, height / 5 * 4 + height / 40);
+    pop();
+  }
 }
