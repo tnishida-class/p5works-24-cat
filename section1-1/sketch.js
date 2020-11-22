c44bb9152faf3d7cb3f6c08c22d3e30a5c797e4b
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-23 08:56:12 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -15,4 +15,9 @@ function widowResized(){
 function draw(){
   image(img1, width / 7, height / 5, height / 5 * 2, height / 5 * 2);
   image(img2, width / 7 * 4, height / 5, height / 5 * 2, height / 5 * 2);
+  rect(width / 7, height / 5 / 3 * 2, width / 7 * 2, height / 15);
+  rect(width / 7 * 4, height / 5 / 3 * 2, width / 7 * 2, height / 15);
+  fill(0, 200, 150);
+  rect(width / 7, height / 5 / 3 * 2, width / 7 * 2, height / 15);
+  rect(width / 7 * 4, height / 5 / 3 * 2, width / 7 * 2, height / 15);
 }
