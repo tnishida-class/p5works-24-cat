3f9ee98b7f6a078ab04ad73d98c99b068224f59b
24-cat <72426620+24-cat@users.noreply.github.com>
2020-11-23 17:39:23 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -27,12 +27,49 @@ function draw(){
   line(width / 7 * 4, height / 10 * 7, width / 7 * 6, height / 10 * 7); // 区切る線man
   line(width / 7 * 5, height / 5 * 3, width / 7 * 5, height / 5 * 4);
   textSize(height / 30)
-  text("   A: 乱れひっかき", width / 7, height / 5 * 3 + height / 30 * 2); // cat
+  text("   A: 乱れひっかき", width / 7, height / 5 * 3 + height / 30 * 2); // cat skill
   text("   B: ネコのいかり", width / 7 * 2, height / 5 * 3 + height / 30 * 2);
   text("  C: はかいこうせん", width / 7, height / 10 * 7  + height / 30 * 2);
   text("   D: ねこぱんち", width / 7 * 2, height / 10 * 7 + height / 30 * 2);
-  text("   A: 正論", width / 7 * 4, height / 5 * 3 + height / 30 * 2); // man
-  text("  B: 冷静な切り返し", width / 7 * 5, height / 5 * 3 + height / 30 * 2);
-  text("  C: はかいこうせん", width / 7 * 4, height / 10 * 7  + height / 30 * 2);
-  text("  D: 銃撃", width / 7 * 5, height / 10 * 7 + height / 30 * 2);
+  text("   E: 正論", width / 7 * 4, height / 5 * 3 + height / 30 * 2); // man skill
+  text("  F: 冷静な切り返し", width / 7 * 5, height / 5 * 3 + height / 30 * 2);
+  text("  G: はかいこうせん", width / 7 * 4, height / 10 * 7  + height / 30 * 2);
+  text("  H: 銃撃", width / 7 * 5, height / 10 * 7 + height / 30 * 2);
+  keyPressed();
+}
+
+function keyPressed(){
+  if(key == "a"){
+    push();
+    fill(0);
+    rect(width / 7, height / 5 * 3, width / 7, height / 10);
+    stroke(255);
+    text("   A: 乱れひっかき", width / 7, height / 5 * 3 + height / 30 * 2);
+    pop();
+  }
+  else if(key == "b"){
+    push();
+    fill(0);
+    rect(width / 7 * 2, height / 5 * 3, width / 7, height / 10);
+    fill(255);
+    strokeWeight(5);
+    text("   B: ネコのいかり", width / 7 * 2, height / 5 * 3 + height / 30 * 2);
+    pop();
+  }
+}
+
+function arrow(x, y){ // 矢印はこれで
+  beginShape();
+  push();
+  noStroke();
+  vertex(x, y);
+  vertex(x + width / 20, y);
+  vertex(x + width / 20, y + height / 40);
+  vertex(x + width / 40 * 3, y - height / 40); // 矢印の先端
+  vertex(x + width / 20, y - height / 40 * 3);
+  vertex(x + width / 20, y - height / 20);
+  vertex(x, y - height / 20);
+  vertex(x, y);
+  endShape();
+  pop();
 }
